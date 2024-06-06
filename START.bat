84128a1f721c0f07b8899e24626a56d9e5dfbfbb
Kevin Wicklein <162309115+KevinWicklein@users.noreply.github.com>
2024-06-06 15:17:30 +0200

    Update START.bat

--- START.bat
+++ START.bat
@@ -1,2 +1,2 @@
 @echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "".\administrative_Freigaben_anpassen.ps1""' -Verb RunAs}"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "".\Bluethooth Deaktivieren.ps1""' -Verb RunAs}"
